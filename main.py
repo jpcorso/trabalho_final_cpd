59e06433d27a248f3d09d2a854d1cb294b97c00d
João Pedro Licks Corso <jp.corso123@gmail.com>
2023-02-28 23:54:22 -0300

    arquivos iniciais

---
+++ main.py
@@ -0,0 +1,8 @@
+#Trabalho final da disciplina de Classificação e Pesquisa de Dados
+#*****************************************************************
+#João Pedro Licks Corso - 00337569********************************
+#Arthur Ferreira Ely - 00XXXXXX***********************************
+
+import functions    #importa biblioteca com as funções do código
+import basedosdados as bd
+
