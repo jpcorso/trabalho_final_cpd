7f63e65b48eda0fcc4010c3715bc21810fcbd260
João Pedro Licks Corso <jp.corso123@gmail.com>
2023-04-08 00:27:40 -0300

    comeco da main

--- main.py
+++ main.py
@@ -4,27 +4,49 @@
 #Arthur Ferreira Ely - 00338434***********************************
 
 import functions as fun    #importa arquivo com todas as funções necessarias
-
+import getPartida as getPart
+import teamHistory as TH
+import rankings as RK
+import arbitros_page as ARB
 #importa arquivos py que criam os arquivos binarios
-import arbitros.getArbitrosFromRawData as arbitros
-import getLocaisFromRawData as locais
-
-
-import openpyxl
-import json
 import pandas as pd
 
-brasileirao = 'brasileiraoSerieA.xlsx'
+def opcao_um():
+    TH.teamHistory()
 
-#brasileirao = pd.read_excel('brasileiraoSerieA.xls')
-#brasileirao.to_excel('brasileiraoSerieA.xlsx', index=False)  #converte xls para xlsx
+def opcao_dois():
+    print("Opção 2 selecionada.")
 
-wbBrasileirao = openpyxl.load_workbook(brasileirao) #coloca a variavel em workbook (arquivo é como se fosse um livro)
-wsBrasileirao = wbBrasileirao['Sheet1'] #como se estivesse escolhendo a página de um linvro, no caso uma aba do excel
-matBrasileirao = list(wsBrasileirao)    #para trabalharmos com matrizes ao invés de 'celulas'
+def opcao_tres():
+    ARB.arbitros()
 
-listaDados = []
+def opcao_quatro():
+    RK.rankings()
 
-fun.colocaTupla(matBrasileirao, listaDados)
 
-#chama escrita de arquivos
+while True:
+    # SWITCH
+    print("futeTUDO - Saiba tudo sobre o mundo da bola")
+    print("-------------------------------------------")
+    print("o que você deseja saber?")
+    print("1 - Históricos")
+    print("2 - Confrontos")
+    print("3 - Árbitros")
+    print("4 - Rankings")
+    print("5 - Sair")
+    opcao = input("Digite o número da opção desejada (1, 2, 3 ou 4): ")
+
+    # Executa a opção selecionada pelo usuário
+    if opcao == "1":
+        opcao_um()
+    elif opcao == "2":
+        opcao_dois()
+    elif opcao == "3":
+        opcao_tres()
+    elif opcao == "4":
+        opcao_quatro()
+    elif opcao == "5":
+        print("Encerrando programa...")
+        break
+    else:
+        print("Opção inválida. Digite apenas 1, 2, 3 ou 4.")
--- rankings.py
+++ rankings.py
@@ -1,97 +1,155 @@
 import pickle
 import getPartida as fun
 
-with open('./rankings/publico.bin', 'rb') as publico_file:
-    # carrega o conteúdo do arquivo para a memória
-    publicos = pickle.load(publico_file);
-    print("Ranking públicos:")
-    for publico in publicos:
-        partida = fun.getPartida(publico)
-        print(f'\n{partida["time_man"]} {partida["gols_man"]} x {partida["gols_vis"]} {partida["time_vis"]}')
-        print(f'Público: {partida["publico"]}')
-        print(f'Estádio: {partida["estadio"]}')
-        print(f'Data: {partida["data"].strftime("%d/%m/%Y")}\n')
+def rankings():
+    print("Rankings do Brasileirão do período de 2003 até 2020...\n")
 
-with open('./rankings/gols.bin', 'rb') as gols_file:
-    # carrega o conteúdo do arquivo para a memória
-    gols = pickle.load(gols_file);
-    print("\nRanking gols:")
-    print(gols)
+    with open('./rankings/publico.bin', 'rb') as publico_file:
+        # carrega o conteúdo do arquivo para a memória
+        publicos = pickle.load(publico_file);
+        print("\nRanking públicos:")
+        print("*************************************")
+        for publico in publicos:
+            partida = fun.getPartida(publico)
+            print(f'\n{partida["time_man"]} {partida["gols_man"]} x {partida["gols_vis"]} {partida["time_vis"]}')
+            print(f'Público: {partida["publico"]}')
+            print(f'Estádio: {partida["estadio"]}')
+            print(f'Data: {partida["data"].strftime("%d/%m/%Y")}\n')
+            print("-------------------------------------")
 
-with open('./rankings/vitorias.bin', 'rb') as vitorias_file:
-    # carrega o conteúdo do arquivo para a memória
-    vitorias = pickle.load(vitorias_file);
-    print("\nRanking vitorias:")
-    print(vitorias)
+    with open('./rankings/gols.bin', 'rb') as gols_file:
+        # carrega o conteúdo do arquivo para a memória
+        gols = pickle.load(gols_file);
+        print("\nRanking gols:")
+        print("*************************************")
+        count = 1 # inicializa a variável contadora
+        for time, gol in gols:
+            if count <= 10: # verifica se a contagem ainda não chegou a 10
+                print(f'{count}º: {time}')
+                print(f'{gol} gols\n')
+                print("-------------------------------------")
+                count += 1 # incrementa a contagem
+            else:
+                break # interrompe o loop quando a contagem chega a 10
 
-with open('./rankings/derrotas.bin', 'rb') as derrotas_file:
-    # carrega o conteúdo do arquivo para a memória
-    derrotas = pickle.load(derrotas_file);
-    print("\nRanking derrotas:")
-    print(derrotas)
+    with open('./rankings/vitorias.bin', 'rb') as vitorias_file:
+        # carrega o conteúdo do arquivo para a memória
+        vitorias = pickle.load(vitorias_file);
+        print("\nRanking vitorias:")
+        print("*************************************")
+        count = 1
+        for time, vitoria in vitorias:
+            if count <= 10:
+                print(f'{count}º: {time}')
+                print(f'{vitoria} vitórias')
+                print("-------------------------------------")
+                count += 1
+            else:
+                break
 
-with open('./rankings/media_idade_maior.bin', 'rb') as media_idade_maior_file:
-    # carrega o conteúdo do arquivo para a memória
-    media_idade_maior = pickle.load(media_idade_maior_file);
-    print("\nTimes mais velhos:")
-    print(media_idade_maior)
+    with open('./rankings/derrotas.bin', 'rb') as derrotas_file:
+        # carrega o conteúdo do arquivo para a memória
+        derrotas = pickle.load(derrotas_file);
+        print("\nRanking derrotas:")
+        print("*************************************")
+        count = 1
+        for time, derrota in derrotas:
+            if count <= 10:
+                print(f'{count}º: {time}')
+                print(f'{derrota} derrotas\n')
+                print("-------------------------------------")
+                count += 1
+            else:
+                break
 
-with open('./rankings/media_idade_menor.bin', 'rb') as media_idade_menor_file:
-    # carrega o conteúdo do arquivo para a memória
-    media_idade_menor = pickle.load(media_idade_menor_file);
-    print("\nTimes mais jovens:")
-    print(media_idade_menor)
+    with open('./rankings/media_idade_maior.bin', 'rb') as media_idade_maior_file:
+        # carrega o conteúdo do arquivo para a memória
+        media_idade_maior = pickle.load(media_idade_maior_file);
+        print("\nTimes mais velhos:")
+        print("*************************************")
+        count = 1
+        for time, media, data in media_idade_maior:
+            if count <= 10:
+                print(f'{count}º: {time}')
+                print(f'Data: {data.strftime("%d/%m/%Y")}')
+                print(f'{media} anos\n')
+                print("-------------------------------------")
+                count += 1
+            else:
+                break
 
-with open('./rankings/mais_caros.bin', 'rb') as mais_caros_file:
-    # carrega o conteúdo do arquivo para a memória
-    mais_caros = pickle.load(mais_caros_file);
-    print("\nTimes mais caros:")
-    i = 0
-    for time, flag in mais_caros:
-        partida = fun.getPartida(time)
-        if flag == "m":
-            time_caro = partida["time_man"]
-            valor = partida["valor_equipe_titular_man"]
-            valorAdversario = partida["valor_equipe_titular_vis"]
-        if flag == "v":
-            time_caro = partida["time_vis"]
-            valor = partida["valor_equipe_titular_vis"]
-            valorAdversario = partida["valor_equipe_titular_man"]
-        i+=1
+    with open('./rankings/media_idade_menor.bin', 'rb') as media_idade_menor_file:
+        # carrega o conteúdo do arquivo para a memória
+        media_idade_menor = pickle.load(media_idade_menor_file);
+        print("\nTimes mais jovens:\n")
+        print("*************************************")
+        count = 1
+        for time, media, data in media_idade_menor:
+            if count <= 10:
+                print(f'{count}º: {time}')
+                print(f'Data: {data.strftime("%d/%m/%Y")}')
+                print(f'{media} anos\n')
+                print("-------------------------------------")
+                count += 1
+            else:
+                break
 
-        valor = "{:,d}".format(valor).replace(",", ".")
-        valorAdversario = "{:,d}".format(valorAdversario).replace(",", ".")
+    with open('./rankings/mais_caros.bin', 'rb') as mais_caros_file:
+        # carrega o conteúdo do arquivo para a memória
+        mais_caros = pickle.load(mais_caros_file);
+        print("\nTimes mais caros:")
+        count = 1
+        for time, flag in mais_caros:
+            if count <= 10:
+                partida = fun.getPartida(time)
+                if flag == "m":
+                    time_caro = partida["time_man"]
+                    valor = partida["valor_equipe_titular_man"]
+                    valorAdversario = partida["valor_equipe_titular_vis"]
+                if flag == "v":
+                    time_caro = partida["time_vis"]
+                    valor = partida["valor_equipe_titular_vis"]
+                    valorAdversario = partida["valor_equipe_titular_man"]
 
-        print(f'\nTime: {time_caro}, Valor: R${valor}')
-        print("-------------------------------------")
-        print(f'{partida["time_man"]} {partida["gols_man"]} x {partida["gols_vis"]} {partida["time_vis"]}')
-        print(f'Valor da equipe Adversária: R${valorAdversario}')
-        print(f'Data: {partida["data"].strftime("%d/%m/%Y")}\n')
+                valor = "{:,d}".format(valor).replace(",", ".")
+                valorAdversario = "{:,d}".format(valorAdversario).replace(",", ".")
 
-with open('./rankings/mais_baratos.bin', 'rb') as mais_baratos_file:
-    # carrega o conteúdo do arquivo para a memória
-    mais_baratos = pickle.load(mais_baratos_file);
-    print("\nTimes mais baratos:")
-    i = 0
-    for time, flag in mais_baratos:
-        partida = fun.getPartida(time)
-        if flag == "m":
-            time_barato = partida["time_man"]
-            valor = partida["valor_equipe_titular_man"]
-            valorAdversario = partida["valor_equipe_titular_vis"]
-        if flag == "v":
-            time_barato = partida["time_vis"]
-            valor = partida["valor_equipe_titular_vis"]
-            valorAdversario = partida["valor_equipe_titular_man"]
-        i+=1
+                print(f'\n{count}º-Time: {time_caro}, Valor: R${valor}')
+                print("-------------------------------------")
+                print(f'{partida["time_man"]} {partida["gols_man"]} x {partida["gols_vis"]} {partida["time_vis"]}')
+                print(f'Valor da equipe Adversária: R${valorAdversario}')
+                print(f'Data: {partida["data"].strftime("%d/%m/%Y")}\n')
+                count += 1
+            else: 
+                break
 
-        valor = "{:,d}".format(valor).replace(",", ".")
-        valorAdversario = "{:,d}".format(valorAdversario).replace(",", ".")
+    with open('./rankings/mais_baratos.bin', 'rb') as mais_baratos_file:
+        # carrega o conteúdo do arquivo para a memória
+        mais_baratos = pickle.load(mais_baratos_file);
+        print("\nTimes mais baratos:")
+        count = 1
+        for time, flag in mais_baratos:
+            if count <= 10:
+                partida = fun.getPartida(time)
+                if flag == "m":
+                    time_barato = partida["time_man"]
+                    valor = partida["valor_equipe_titular_man"]
+                    valorAdversario = partida["valor_equipe_titular_vis"]
+                if flag == "v":
+                    time_barato = partida["time_vis"]
+                    valor = partida["valor_equipe_titular_vis"]
+                    valorAdversario = partida["valor_equipe_titular_man"]
 
-        print(f'\nTime: {time_barato}, Valor: R${valor}')
-        print("-------------------------------------")
-        print(f'{partida["time_man"]} {partida["gols_man"]} x {partida["gols_vis"]} {partida["time_vis"]}')
-        print(f'Valor da equipe Adversária: R${valorAdversario}')
-        print(f'Data: {partida["data"].strftime("%d/%m/%Y")}\n')
+                valor = "{:,d}".format(valor).replace(",", ".")
+                valorAdversario = "{:,d}".format(valorAdversario).replace(",", ".")
+
+                print(f'\n{count}º-Time: {time_barato}, Valor: R${valor}')
+                print("-------------------------------------")
+                print(f'{partida["time_man"]} {partida["gols_man"]} x {partida["gols_vis"]} {partida["time_vis"]}')
+                print(f'Valor da equipe Adversária: R${valorAdversario}')
+                print(f'Data: {partida["data"].strftime("%d/%m/%Y")}\n')
+                count += 1
+            else:
+                break
 
 
