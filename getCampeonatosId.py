2cc490d337fb34608c265c09c9ab892d043964c9
ArthurEly <arthur.ely78@gmail.com>
2023-04-08 16:06:49 -0300

    criando os indices dos campeonatos

--- getCampeonatosId.py
+++ getCampeonatosId.py
@@ -5,16 +5,25 @@ def createFile():
     temPartidas = True
     i=0;
     ids_campeonatos = {}
+    ano_atual = 2003;
     while temPartidas:
         try:
             partida = fun.getPartida(i);
             ano = partida["ano_campeonato"]
             if ano not in ids_campeonatos:
-                ids_campeonatos[ano] = partida["id"]
+                ids_campeonatos[ano] = [partida["id"]]
+            if(ano_atual != ano):
+                ids_campeonatos[ano_atual].append(partida["id"])
+                ano_atual = ano
             i+=1;
         except IndexError as e:
             print("Sem mais partidas.");
+            ids_campeonatos[ano_atual].append(i-1)
             temPartidas = False;
     
-    with open('./indices_arquivos/ano_campeonatos.bin', 'wb') as f:
+    print(ids_campeonatos)
+
+    with open('./indices_arquivos/indices_ano_campeonatos.bin', 'wb') as f:
         pickle.dump(ids_campeonatos,f)
+
+createFile()
